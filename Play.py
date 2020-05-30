1bf3a3bde4734d48b32ed9e7949da1fff5916158
Brandt Green <153144Green@chsbr.net>
2020-05-30 11:55:55 -0500

    Initial commit

---
+++ Play.py
@@ -0,0 +1,5 @@
+import ciphy
+
+message = ciphy.caesar_encrypt('Hi my name is brandy', 2)
+print(message)
+
---
+++ ciphy.py
@@ -0,0 +1,30 @@
+"""Cipher stuff"""
+
+
+def reverse(string_input):
+    """Reverses a string"""
+
+    letter_count = len(string_input)
+    backwards = ''
+    x = 1
+    while x <= letter_count:
+        backwards += string_input[-x]
+        x += 1
+    return backwards
+
+
+def caesar_encrypt(message, key):
+    """Encrypt a message with the provided key"""
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    message = message.upper()
+    encrypted_message = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num += key
+            if num > len(LETTERS) - 1:
+                num -= len(LETTERS)
+            encrypted_message += LETTERS[num]
+        else:
+            encrypted_message += symbol
+    return encrypted_message
