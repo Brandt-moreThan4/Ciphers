4d2cd7f07f28946df2b6f16ffe1950681670c468
Brandt Green <153144Green@chsbr.net>
2020-06-01 22:54:34 -0500

    Reorganizing some files

--- TranspositionCipherStuff/transpositionCipher.py
+++ TranspositionCipherStuff/transpositionCipher.py
@@ -35,32 +35,8 @@ def decrypt_message(secret_message, key):
     return ''.join(decrypted_grid)
 
 
-def test_bunch():
-    """Use this to test the encrypt and decrypt functions"""
-    rd.seed(42)
-
-    for i in range(20):
-        # Random list of letters to test
-        message = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ" * rd.randint(3, 20))
-        rd.shuffle(message)
-        message = ''.join(message)
-        print(f'Test #{i + 1}: {message}')
-
-        # Test with all the possible keys
-        for key in range(1, len(message)):
-            encrypted = encrypt_message(message, key)
-            decrypted = decrypt_message(encrypted, key)
-
-            if message != decrypted:
-                print(f'Problem with test number {i} key: {key}')
-                sys.exit()
-
-    print('Done with testing')
-
-
 def main():
-    print('About to start testing')
-    test_bunch()
+    pass
 
 
 if __name__ == '__main__':
